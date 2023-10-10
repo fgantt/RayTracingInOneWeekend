7a93688cf0d7a3417539faa5ee717bbd43613f47
Floyd Gantt <fgantt@gmail.com>
2023-10-09 22:35:52 -0500

    ch 6.7

--- ch06/ray-tracer/main.go
+++ ch06/ray-tracer/main.go
@@ -4,30 +4,35 @@ import (
 	"fmt"
 	"log"
 	"math"
-
 	"vec3/vec3"
 )
 
-func hitSphere(center vec3.Point3, radius float64, r vec3.Ray) float64 {
-	oc := r.Origin().Sub(center.Vec3)
-	a := r.Direction().LengthSquared()
-	halfB := vec3.Dot(oc, r.Direction())
-	c := oc.LengthSquared() - radius*radius
-	discriminant := halfB*halfB - a*c
+//func hitSphere(center vec3.Point3, radius float64, r vec3.Ray) float64 {
+//	oc := r.Origin().Sub(center.Vec3)
+//	a := r.Direction().LengthSquared()
+//	halfB := vec3.Dot(oc, r.Direction())
+//	c := oc.LengthSquared() - radius*radius
+//	discriminant := halfB*halfB - a*c
+//
+//	if discriminant < 0 {
+//		return -1.0
+//	}
+//	return (-halfB - math.Sqrt(discriminant)) / a
+//}
 
-	if discriminant < 0 {
-		return -1.0
-	}
-	return (-halfB - math.Sqrt(discriminant)) / a
-}
-
-func rayColor(r vec3.Ray) vec3.Color {
-	t := hitSphere(vec3.NewPoint3(0, 0, -1), 0.5, r)
-	if t > 0.0 {
-		N := vec3.UnitVector(r.At(t).Vec3.Sub(vec3.New(0, 0, -1)))
-		v := vec3.New(N.X()+1, N.Y()+1, N.Z()+1).Mul(0.5)
+func rayColor(r vec3.Ray, world vec3.Hittable) vec3.Color {
+	isHit, hitRec := world.Hit(r, 0, math.Inf(1))
+	if isHit {
+		v := hitRec.Normal().Add(vec3.NewColor(1, 1, 1).Vec3).Mul(0.5)
 		return vec3.NewColor(v.X(), v.Y(), v.Z())
 	}
+
+	//t := hitSphere(vec3.NewPoint3(0, 0, -1), 0.5, r)
+	//if t > 0.0 {
+	//	N := vec3.UnitVector(r.At(t).Vec3.Sub(vec3.New(0, 0, -1)))
+	//	v := vec3.New(N.X()+1, N.Y()+1, N.Z()+1).Mul(0.5)
+	//	return vec3.NewColor(v.X(), v.Y(), v.Z())
+	//}
 
 	unitDirection := vec3.UnitVector(r.Direction())
 	a := 0.5 * (unitDirection.Y() + 1.0)
@@ -48,6 +53,12 @@ func main() {
 	if imageHeight < 1 {
 		imageHeight = 1
 	}
+
+	// World
+
+	world := vec3.HittableList{}
+	world.Add(vec3.NewSphere(vec3.NewPoint3(0, 0, -1), 0.5))
+	world.Add(vec3.NewSphere(vec3.NewPoint3(0, -100.5, -1), 100))
 
 	// Camera
 
@@ -83,7 +94,7 @@ func main() {
 			rayDirection := pixelCenter.Sub(cameraCenter.Vec3)
 			r := vec3.NewRay(cameraCenter, rayDirection)
 
-			pixelColor := rayColor(r)
+			pixelColor := rayColor(r, world)
 			fmt.Print(pixelColor.Write())
 		}
 	}
---
+++ ch06/ray-tracer/vec3/util.go
@@ -0,0 +1,7 @@
+package vec3
+
+import "math"
+
+func DegreesToRadians(degrees float64) float64 {
+	return degrees * math.Pi / 180.0
+}
