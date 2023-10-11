f6c61473f28631087a98a51fd145a025e06374e4
Floyd Gantt <fgantt@gmail.com>
2023-10-10 21:22:51 -0500

    ch 7

---
+++ ch08/ray-tracer/vec3/interval.go
@@ -0,0 +1,27 @@
+package vec3
+
+import "math"
+
+type Interval struct {
+	min float64
+	max float64
+}
+
+func NewInterval(min float64, max float64) Interval {
+	return Interval{min, max}
+}
+
+func (i Interval) Min() float64 { return i.min }
+func (i Interval) Max() float64 { return i.max }
+
+func DefaultInterval() Interval {
+	return Interval{math.Inf(+1), math.Inf(-1)}
+}
+
+func (i Interval) Contains(x float64) bool {
+	return i.min <= x && x <= i.max
+}
+
+func (i Interval) Surrounds(x float64) bool {
+	return i.min < x && x < i.max
+}
