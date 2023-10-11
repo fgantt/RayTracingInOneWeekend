cf63042e32d92962f66e28c168160fdd90207df3
Floyd Gantt <fgantt@gmail.com>
2023-10-11 16:42:20 -0500

    ch 9.4

--- ch09/ray-tracer/camera/camera.go
+++ ch09/ray-tracer/camera/camera.go
@@ -118,7 +118,7 @@ func rayColor(r vec3.Ray, depth int, world vec3.Hittable) vec3.Color {
 	}
 	isHit, hitRec := world.Hit(r, vec3.NewInterval(0.001, math.Inf(1)))
 	if isHit {
-		direction := vec3.RandomOnHemisphere(hitRec.Normal())
+		direction := hitRec.Normal().Add(vec3.RandomUnitVector())
 		tempV := rayColor(vec3.NewRay(hitRec.P(), direction), depth-1, world).Mul(0.5)
 		return vec3.NewColor(tempV.X(), tempV.Y(), tempV.Z())
 	}
